8da460b19d0a46950844b996fc92d998778ad05c
akshay-parande <147257172+akshay-parande@users.noreply.github.com>
2025-04-19 08:25:14 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,12 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_connection():
-    return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASS"),
-        port=os.getenv("DB_PORT", 5432)  # Default PostgreSQL port
+    return psycopg2.connect(os.environ['DATABASE_URL'])  # Default PostgreSQL port
     )
 
 
